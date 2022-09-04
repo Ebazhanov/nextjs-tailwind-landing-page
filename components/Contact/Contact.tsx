c260e3684961368583fa5c1a31bfc3a624722efb
ebazhanov <evg.bazhanov@yahoo.com>
2022-09-04 20:46:56 +0200

    improve mobile view for contact form

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const contact = () => {
   return (
-    <div id="contact" className="max-w-[1240px] m-auto p-44 h-screen">
+    <div className="max-w-[1240px] m-auto p-4 h-screen">
       <h1 className="text-2xl font-bold text-center p-4">
         Send me a 🏃 message
       </h1>
@@ -26,6 +26,8 @@ const contact = () => {
         />
         <textarea
           className="border shadow-lg p-3 w-full"
+          cols={30}
+          rows={10}
           placeholder="Message"
         ></textarea>
         <button className="border shadow-lg p-3 w-full mt-2">Submit</button>
